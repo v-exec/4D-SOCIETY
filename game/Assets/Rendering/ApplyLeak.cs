a142e5a62b4a548028123b6608360e08659632dc
vi <reducedprocess@gmail.com>
2019-10-09 11:39:48 -0400

    add preliminary melt shader

---
+++ game/Assets/Rendering/ApplyLeak.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Cam.Effects {
+	
+	[ExecuteInEditMode]
+	[RequireComponent(typeof(UnityEngine.Camera))]
+	[AddComponentMenu("Image Effects/Custom/Leak")]
+
+	public class ApplyLeak : MonoBehaviour {
+		private Material m_material;
+		private Shader shader;
+        private RenderTexture accumTexture0;
+        private RenderTexture accumTexture1;
+        private Material material;
+
+        bool currentTexture = false;
+
+		public void OnRenderImage(RenderTexture source, RenderTexture destination) {
+
+            // create the accumulation textures
+            if (accumTexture0 == null || accumTexture0.width != source.width || accumTexture0.height != source.height) {
+                DestroyImmediate(accumTexture0);
+                accumTexture0 = new RenderTexture(source.width, source.height, 0);
+                accumTexture0.hideFlags = HideFlags.HideAndDontSave;
+                Graphics.Blit(source, accumTexture0);
+            }
+            if (accumTexture1 == null || accumTexture1.width != source.width || accumTexture1.height != source.height) {
+                DestroyImmediate(accumTexture1);
+                accumTexture1 = new RenderTexture(source.width, source.height, 0);
+                accumTexture1.hideFlags = HideFlags.HideAndDontSave;
+                Graphics.Blit(source, accumTexture1);
+            }
+
+            // create the material
+            if (material == null) {
+                shader = Shader.Find("Leak");
+                material = new Material(shader) { hideFlags = HideFlags.DontSave };
+                // set the overlaying texture
+                material.SetTexture("_SourceTex", source);
+            }
+
+            // double buffering the accumulation textures
+            if (currentTexture) {
+                // distort accumTexture0 and overlay source into accumTexture1
+                Graphics.Blit(accumTexture0, accumTexture1, material);
+                // send out accumTexture1
+                Graphics.Blit(accumTexture1, destination);
+            } else {
+                // distort accumTexture0 and overlay source into accumTexture1
+                Graphics.Blit(accumTexture1, accumTexture0, material);
+                // send out accumTexture1
+                Graphics.Blit(accumTexture0, destination);
+            }
+            // update the current texture for the next iteration
+            currentTexture = !currentTexture;
+        }
+    }
+}
---
+++ game/Assets/Rendering/Leak.shader
@@ -0,0 +1,64 @@
+﻿Shader "Leak" {
+	Properties {
+	 	_MainTex ("", 2D) = "white" {}
+	}
+	SubShader {
+        Tags { "Queue"="Transparent" "RenderType"="Transparent" "IgnoreProjector"="True" }
+        
+	 	Pass {
+            // distort previous accumulation buffer and overwrite into current accumulation buffer
+
+            // TODO: get a better distortion vector field, either procedural or as a texture
+
+            Blend Off
+
+	  		CGPROGRAM
+	  		#pragma vertex vert_img
+	  		#pragma fragment frag
+	  		#include "UnityCG.cginc"
+            #include "ClassicNoise3D.hlsl"
+
+            static const float PI = 3.14159;
+
+	  		uniform sampler2D _MainTex;
+            uniform sampler2D _SourceTex; 
+
+            float2 remap(float2 coord) {
+                //return coord + 0.01 * float2(sin(coord.x * PI * 5), sin(coord.y * PI * 5));
+                return coord + 0.01 * float2(
+                    cnoise(float3(5.0 * coord, _Time.y * 0.5)), 
+                    cnoise(float3(5.0 * (coord + float2(1.0, 1.0)), _Time.y * 0.5))
+                );
+            }
+
+	  		fixed4 frag(v2f_img i) : COLOR {
+			    return 0.95 * fixed4(tex2D(_MainTex, remap(i.uv)).rgba);
+	  		}
+	  		ENDCG
+	 	}
+
+        Pass {
+            // copy camera image over current accumulation buffer with alpha
+
+            // TODO: add sky texture or color input
+            
+            // original, liquid inside object, preserves background alpha
+            // Blend SrcAlpha OneMinusSrcAlpha
+
+            // properly working, liquid trailing off object, does not preserve background alpha
+            Blend SrcAlpha One 
+
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+            
+            uniform sampler2D _SourceTex;
+
+            fixed4 frag(v2f_img i) : COLOR {
+                return fixed4(tex2D(_SourceTex, i.uv).rgba);
+            }
+            ENDCG
+        }
+	}
+}
