919c9e4b4d117c2c0b6ac19093534c6a88beb0a3
vi <reducedprocess@gmail.com>
2019-11-06 11:25:28 -0500

    add level design blocks (textures pending)

--- 4D SOCIETY/Assets/Environment/Glitch/ApplyLeak.cs
+++ 4D SOCIETY/Assets/Environment/Glitch/ApplyLeak.cs
@@ -1,34 +1,87 @@
 ﻿using UnityEngine;
 
 namespace Assets.Scripts.Cam.Effects {
-	
-	[ExecuteInEditMode]
-	[RequireComponent(typeof(UnityEngine.Camera))]
-	[AddComponentMenu("Image Effects/Custom/Leak")]
+    
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(UnityEngine.Camera))]
+    [AddComponentMenu("Image Effects/Custom/Leak")]
 
-	public class ApplyLeak : MonoBehaviour {
-		private Material m_material;
-		private Shader shader;
+    public class ApplyLeak : MonoBehaviour {
+        private Material m_material;
+        private Shader shader;
         private RenderTexture accumTexture0;
         private RenderTexture accumTexture1;
+        private RenderTexture accumTexture2;
+        private RenderTexture accumTexture3;
         private Material material;
 
-        bool currentTexture = false;
+        bool currentTextureEye0 = false;
+        bool currentTextureEye1 = false;
+        bool currentEye = false;
 
-		public void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        public int antiAliasing = 1;
+        public float scaleDistortion = 8.0f;
+        public float scaleFlow = 2.0f;
+        public float scaleTurbulence = 60.0f;
+        public float speedDistortion = 0.5f;
+        public float speedFlow = 0.2f;
+        public float speedTurbulence = 5.8f;
+        public float amountDistortion = 0.1f;
+        public float amountFlow = 2.5f;
+        public float amountTurbulence = 0.01f;
+
+        private void SanitizeShaderParameters() {
+            antiAliasing =      Mathf.Clamp(antiAliasing, 1, 2048);
+            scaleDistortion =   Mathf.Clamp(scaleDistortion, 0.0f, 100.0f);
+            scaleFlow =         Mathf.Clamp(scaleFlow, 0.0f, 100.0f);
+            scaleTurbulence =   Mathf.Clamp(scaleTurbulence, 0.0f, 100.0f);
+            speedDistortion =   Mathf.Clamp(speedDistortion, 0.0f, 100.0f);
+            speedFlow =         Mathf.Clamp(speedFlow, 0.0f, 100.0f);
+            speedTurbulence =   Mathf.Clamp(speedTurbulence, 0.0f, 100.0f);
+            amountDistortion =  Mathf.Clamp(amountDistortion, 0.0f, 100.0f);
+            amountFlow =        Mathf.Clamp(amountFlow, 0.0f, 100.0f);
+            amountTurbulence =  Mathf.Clamp(amountTurbulence, 0.0f, 100.0f);
+        }
+
+        private void SendShaderParameters() {
+            material.SetInt("antiAliasing", antiAliasing);
+            material.SetFloat("scaleDistortion", scaleDistortion);
+            material.SetFloat("scaleFlow", scaleFlow);
+            material.SetFloat("scaleTurbulence", scaleTurbulence);
+            material.SetFloat("speedDistortion", speedDistortion);
+            material.SetFloat("speedFlow", speedFlow);
+            material.SetFloat("speedTurbulence", speedTurbulence);
+            material.SetFloat("amountDistortion", amountDistortion);
+            material.SetFloat("amountFlow", amountFlow);
+            material.SetFloat("amountTurbulence", amountTurbulence);
+        }
+
+        public void OnRenderImage(RenderTexture source, RenderTexture destination) {
 
             // create the accumulation textures
-            if (accumTexture0 == null || accumTexture0.width != source.width || accumTexture0.height != source.height) {
+            if (!currentEye && (accumTexture0 == null || accumTexture0.width != source.width || accumTexture0.height != source.height)) {
                 DestroyImmediate(accumTexture0);
                 accumTexture0 = new RenderTexture(source.width, source.height, 0);
                 accumTexture0.hideFlags = HideFlags.HideAndDontSave;
                 Graphics.Blit(source, accumTexture0);
             }
-            if (accumTexture1 == null || accumTexture1.width != source.width || accumTexture1.height != source.height) {
+            if (!currentEye && (accumTexture1 == null || accumTexture1.width != source.width || accumTexture1.height != source.height)) {
                 DestroyImmediate(accumTexture1);
                 accumTexture1 = new RenderTexture(source.width, source.height, 0);
                 accumTexture1.hideFlags = HideFlags.HideAndDontSave;
                 Graphics.Blit(source, accumTexture1);
+            }
+            if (currentEye && (accumTexture2 == null || accumTexture2.width != source.width || accumTexture2.height != source.height)) {
+                DestroyImmediate(accumTexture2);
+                accumTexture2 = new RenderTexture(source.width, source.height, 0);
+                accumTexture2.hideFlags = HideFlags.HideAndDontSave;
+                Graphics.Blit(source, accumTexture2);
+            }
+            if (currentEye && (accumTexture3 == null || accumTexture3.width != source.width || accumTexture3.height != source.height)) {
+                DestroyImmediate(accumTexture3);
+                accumTexture3 = new RenderTexture(source.width, source.height, 0);
+                accumTexture3.hideFlags = HideFlags.HideAndDontSave;
+                Graphics.Blit(source, accumTexture3);
             }
 
             // create the material
@@ -39,20 +92,51 @@ namespace Assets.Scripts.Cam.Effects {
                 material.SetTexture("_SourceTex", source);
             }
 
-            // double buffering the accumulation textures
-            if (currentTexture) {
-                // distort accumTexture0 and overlay source into accumTexture1
-                Graphics.Blit(accumTexture0, accumTexture1, material);
-                // send out accumTexture1
-                Graphics.Blit(accumTexture1, destination);
+            // sanitize & send shader parameters
+            SanitizeShaderParameters();
+            SendShaderParameters();
+
+            if (!currentEye) {
+                // left eye
+                material.SetTexture("_SourceTex", source);
+                // double buffering the accumulation textures
+                if (currentTextureEye0) {
+                    // distort accumTexture0 and overlay source into accumTexture1
+                    Graphics.Blit(accumTexture0, accumTexture1, material);
+                    // send out accumTexture1
+                    Graphics.Blit(accumTexture1, destination);
+                } else {
+                    // distort accumTexture0 and overlay source into accumTexture1
+                    Graphics.Blit(accumTexture1, accumTexture0, material);
+                    // send out accumTexture1
+                    Graphics.Blit(accumTexture0, destination);
+                }
+
+
+                // update the current texture for the next iteration
+                currentTextureEye0 = !currentTextureEye0;
             } else {
-                // distort accumTexture0 and overlay source into accumTexture1
-                Graphics.Blit(accumTexture1, accumTexture0, material);
-                // send out accumTexture1
-                Graphics.Blit(accumTexture0, destination);
+                // right eye
+                // double buffering the accumulation textures
+                material.SetTexture("_SourceTex", source);
+                if (currentTextureEye1) {
+                    // distort accumTexture2 and overlay source into accumTexture3
+                    Graphics.Blit(accumTexture2, accumTexture3, material);
+                    // send out accumTexture3
+                    Graphics.Blit(accumTexture3, destination);
+                } else {
+                    // distort accumTexture3 and overlay source into accumTexture2
+                    Graphics.Blit(accumTexture3, accumTexture2, material);
+                    // send out accumTexture2
+                    Graphics.Blit(accumTexture2, destination);
+                }
+
+                // update the current texture for the next iteration
+                currentTextureEye1 = !currentTextureEye1;
             }
-            // update the current texture for the next iteration
-            currentTexture = !currentTexture;
+
+            // update the current eye for the next iteration
+            currentEye = !currentEye;
         }
     }
 }
--- 4D SOCIETY/Assets/Environment/Glitch/Leak.shader
+++ 4D SOCIETY/Assets/Environment/Glitch/Leak.shader
@@ -1,41 +1,62 @@
 ﻿Shader "Leak" {
-	Properties {
-	 	_MainTex ("", 2D) = "white" {}
-	}
-	SubShader {
+    Properties {
+        _MainTex ("", 2D) = "white" {}
+    }
+    SubShader {
         Tags { "Queue"="Transparent" "RenderType"="Transparent" "IgnoreProjector"="True" }
         
-	 	Pass {
+        Pass {
             // distort previous accumulation buffer and overwrite into current accumulation buffer
 
             // TODO: get a better distortion vector field, either procedural or as a texture
 
             Blend Off
 
-	  		CGPROGRAM
-	  		#pragma vertex vert_img
-	  		#pragma fragment frag
-	  		#include "UnityCG.cginc"
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+            #include "UnityCG.cginc"
             #include "ClassicNoise3D.hlsl"
 
-            static const float PI = 3.14159;
+            static const int antialiasing = 4;
 
-	  		uniform sampler2D _MainTex;
-            uniform sampler2D _SourceTex; 
+            uniform sampler2D _MainTex;
+            uniform sampler2D _SourceTex;
 
-            float2 remap(float2 coord) {
-                //return coord + 0.01 * float2(sin(coord.x * PI * 5), sin(coord.y * PI * 5));
-                return coord + 0.01 * float2(
-                    cnoise(float3(5.0 * coord, _Time.y * 0.5)), 
-                    cnoise(float3(5.0 * (coord + float2(1.0, 1.0)), _Time.y * 0.5))
+            float4 _MainTex_TexelSize;
+
+            float2 distort(float2 coord) {
+                float2 noise;
+                    
+                noise = 0.3 * float2(
+                    cnoise(float3(8.0 * (coord), _Time.y * 0.5)), 
+                    cnoise(float3(8.0 * (coord +  float2(1.0, 1.0)), _Time.y * 0.5))
                 );
+
+                noise = 1.8 * unity_DeltaTime.z * float2(
+                    cnoise(float3(2.0 * (coord + noise), _Time.y * 0.2)), 
+                    cnoise(float3(2.0 * (coord + noise +  float2(1.0, 1.0)), _Time.y * 0.2))
+                );
+
+                return noise;
             }
 
-	  		fixed4 frag(v2f_img i) : COLOR {
-			    return 0.95 * fixed4(tex2D(_MainTex, remap(i.uv)).rgba);
-	  		}
-	  		ENDCG
-	 	}
+            fixed4 frag(v2f_img i) : COLOR {
+            
+                float2 delta = distort(i.uv);
+                float2 coord = i.uv;
+                float norm = length(i.uv);
+
+                fixed4 result = 0;
+
+                for(int count = 0; count < antialiasing; count++) {
+                    result += fixed4(tex2D(_MainTex, coord + delta * (count + 1.0 ) / (antialiasing * 1.0)).rgba);
+                }
+
+                return result / (1.0 * antialiasing) * exp(- unity_DeltaTime.z * 12.0);
+            }
+            ENDCG
+        }
 
         Pass {
             // copy camera image over current accumulation buffer with alpha
@@ -60,5 +81,5 @@
             }
             ENDCG
         }
-	}
+    }
 }
