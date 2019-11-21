0c98cbbf13f8ae33761c94efe72368835de9dca4
vi <reducedprocess@gmail.com>
2019-11-21 04:30:15 -0500

    add discovery mechanic

---
+++ 4D SOCIETY/Assets/Environment/Glitch/Discover.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Discover : MonoBehaviour {
+
+	private Vector4[] points;
+	private int pointCount = 50;
+	private int pointIndex = 0;
+	private bool ready = false;
+	private float pointInterval = 0.2f;
+	private float blenderSpeed = 5f;
+	private GameObject[] meshes;
+	private Coroutine[] routines;
+	private float[] amounts;
+	public LayerMask mask;
+	private float biggestDot = 0.1f;
+	private float smallestDot = 5f;
+
+	void Start() {
+		points = new Vector4[pointCount];
+		routines = new Coroutine[pointCount];
+		amounts = new float[pointCount];
+
+		meshes = GameObject.FindGameObjectsWithTag("Discoverable");
+		StartCoroutine(ReadyCycle());
+	}
+
+	void Update() {
+		//on mouse click at interval,
+		//trigger a new point discovery
+		if (Input.GetMouseButton(0) && ready) {
+			ready = false;
+
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~mask)) {
+				if (hit.collider.tag == "Discoverable") {
+					pointIndex++;
+					if (pointIndex == pointCount) pointIndex = 0;
+
+					points[pointIndex] = new Vector4(hit.point.x, hit.point.y, hit.point.z, 0);
+					if (routines[pointIndex] != null) StopCoroutine(routines[pointIndex]);
+					routines[pointIndex] = StartCoroutine(Blend(pointIndex));
+				}
+			}
+		}
+	}
+
+	IEnumerator ReadyCycle() {
+		while (true) {
+			yield return new WaitForSeconds(pointInterval);
+			ready = true;
+		}
+	}
+
+
+	IEnumerator Blend(int index) {
+		//reset blending amounts
+		amounts[index] = smallestDot;
+		for (int i = 0; i < meshes.Length; i++) {
+			meshes[i].GetComponent<Renderer>().material.SetFloatArray("_Amounts", amounts);
+		}
+
+		//add new discovery point to all meshes
+		for (int i = 0; i < meshes.Length; i++) {
+			meshes[i].GetComponent<Renderer>().material.SetVectorArray("_Discoveries", points);
+		}
+
+		//ease discovery falloff for visual effect
+		while (amounts[index] > biggestDot) {
+			yield return new WaitForSeconds(0.01f);
+			amounts[index] = FourD.ease(amounts[index], biggestDot, blenderSpeed);
+			for (int i = 0; i < meshes.Length; i++) {
+				meshes[i].GetComponent<Renderer>().material.SetFloatArray("_Amounts", amounts);
+			}
+		}
+	}
+}
--- 4D SOCIETY/Assets/Environment/Glitch/DiscoverableMesh.shader
+++ 4D SOCIETY/Assets/Environment/Glitch/DiscoverableMesh.shader
@@ -3,9 +3,11 @@
 		_Color ("Color", Color) = (1,1,1,1)
 		_MainTex ("Texture", 2D) = "white" {}
 		_DissolveTexture("Dissolve Texture", 2D) = "white" {}
-		[PerRendererData]_Amount("Amount", Range(0,1)) = 0
 		_Glossiness ("Smoothness", Range(0,1)) = 0.5
 		_Metallic ("Metallic", Range(0,1)) = 0.0
+
+		_DiscoveryDistance ("DiscoveryDistance", Range(0.5, 6)) = 2.0
+		_PointCount ("PointCount", Int) = 50
 	}
 
 	SubShader {
@@ -14,6 +16,7 @@
 		Cull Off
 
 		CGPROGRAM
+
 		#pragma surface surf Standard fullforwardshadows
 		#pragma target 3.0
 
@@ -21,18 +24,41 @@
 
 		struct Input {
 			float2 uv_MainTex;
+			float3 worldPos;
 		};
 
-		half _Glossiness;
-		half _Metallic;
+		float remap (float value, float from1, float to1, float from2, float to2) {
+		    return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+		}
+
+		float _Glossiness;
+		float _Metallic;
 		fixed4 _Color;
 
 		sampler2D _DissolveTexture;
-		half _Amount;
+		float _DiscoveryDistance;
+		int _PointCount;
+		float _Amounts[50];
+		fixed3 _Discoveries[50];
 
 		void surf (Input IN, inout SurfaceOutputStandard o) {
-			half dissolve_value = tex2D(_DissolveTexture, IN.uv_MainTex).r;
-			clip(dissolve_value - _Amount);
+
+			//sum all relevant points' distance to calculate whether frag should be opaque
+			float collectiveOpacity = 0.0;
+
+			for (int i = 0; i < _PointCount; i++) {
+				float d = distance(IN.worldPos, _Discoveries[i]);
+				if (d < (_DiscoveryDistance * remap(_Amounts[i], 5, 0.1, 0, 1))) {
+					collectiveOpacity += remap(d, 0, _DiscoveryDistance, 1, 0);
+				}
+			}
+
+			//treat the collective opacity to retain noise pattern
+			collectiveOpacity = remap(collectiveOpacity, 0, _DiscoveryDistance * 2, 0, 1);
+
+			//clip
+			float dissolve_value = tex2D(_DissolveTexture, IN.uv_MainTex).r;
+			clip(-dissolve_value + collectiveOpacity);
 
 			fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
 
