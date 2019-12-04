702a0b8bde625b1a689eb62004193378e579dcde
vi <reducedprocess@gmail.com>
2019-12-04 13:48:46 -0500

    quality of life changes (respawn, scene changing)

---
+++ 4D SOCIETY/Assets/Functionality/DeathFix.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathFix : MonoBehaviour {
+	public GameObject spawn;
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			other.transform.position = spawn.transform.position;
+		}
+	}
+}
